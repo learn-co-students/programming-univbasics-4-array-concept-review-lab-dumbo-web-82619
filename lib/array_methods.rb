b02221e60002c2e04c4cad7648df8ab878a308c4
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-22 18:40:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,16 +2,38 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   # use .index() method to find index of the element
   # loop over the array using while method 
-  
-  array.index(value_to_find)
+  array.length.times do |index|
+    if array[index] == value_to_find
+      return index 
+    end
+  end
+  nil 
 end
 
 def find_max_value(array)
   # Add your solution here
-  array.max 
+  highest_number = 0 
+  
+  array.length.times do |index|
+    while array[index] > highest_number
+      highest_number = array[index]
+    end 
+  end 
+  highest_number
 end
 
 def find_min_value(array)
   # Add your solution here
-  array.min
+  lowest_number = 0 
+  
+  array.length.times do |index|
+    if lowest_number == 0 
+      lowest_number = array[index]
+    else 
+      if array[index] < lowest_number 
+        lowest_number = array[index]
+      end
+    end
+  end 
+  lowest_number
 end
