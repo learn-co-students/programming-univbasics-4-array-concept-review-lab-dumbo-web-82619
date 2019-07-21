bd9a316f63ebd7aa2f4c258f2b39fc0ffe64eddf
Benny Louie <bennylouie1412@gmail.com>
2019-07-21 22:16:03 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ end
 
 def find_max_value(array)
   counter = 0
-  max = 0
+  max = array[0]
   while counter < array.length do
     if array[counter] > max
       max = array[counter]
@@ -22,7 +22,7 @@ end
 
 def find_min_value(array)
   counter = 0
-  min = 99
+  min = array[0]
   while counter < array.length do
     if array[counter] < min
       min = array[counter]
