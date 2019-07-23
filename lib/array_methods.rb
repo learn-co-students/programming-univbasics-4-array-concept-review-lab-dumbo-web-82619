cb194cbd64a478b8ca4c940a7bc0032e8a6b303e
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 03:35:19 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,35 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array[counter] do
+    if array[counter] == value_to_find
+      return counter
+    end
+    counter += 1
+  end
+   nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0 
+  counter = 0
+  while array[counter] do
+    if array[counter] > max_value
+      max_value = array[counter]
+    end
+    counter +=1 
+  end
+  max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min_value = array[0]
+  index = 0
+  while array[index] do 
+    if array[index] < min_value
+      min_value = array[index]
+    end
+    index += 1
+  end
+  min_value
 end
+
